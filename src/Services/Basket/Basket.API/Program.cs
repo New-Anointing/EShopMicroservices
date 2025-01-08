215a71a1ed6b7b4f4e72a9cda60b2d61ecebd4b3
New-Anointing Aremu <aremunewanointing@gmail.com>
2025-01-09 00:05:36 +0100

    Docker settings and cache implementation

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,12 @@
 using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 //Add services to container
 var assembly = typeof(Program).Assembly;
 var connectionString = builder.Configuration.GetConnectionString("Database")!;
+var redisConnectionString = builder.Configuration.GetConnectionString("Redis")!;
 builder.Services.AddMediatR(options =>
 {
     options.RegisterServicesFromAssemblies(assembly);
@@ -17,10 +20,23 @@ builder.Services.AddMarten(options =>
 }).UseLightweightSessions();
 builder.Services.AddCarter();
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = redisConnectionString;
+});
 builder.Services.AddValidatorsFromAssembly(assembly);
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+builder.Services.AddHealthChecks()
+    .AddNpgSql(connectionString)
+    .AddRedis(redisConnectionString);
 var app = builder.Build();
 app.UseExceptionHandler(options => { });
 //Configure the HTTP request pipeline
 app.MapCarter();
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 app.Run();
---
+++ src/Services/Basket/Basket.API/Services/CachedBasketRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Services
+{
+    public class CachedBasketRepository(IBasketRepository basketRepository, IDistributedCache cache) : IBasketRepository
+    {
+        public async Task<ShoppingCart> GetBasketAsync(string userName, CancellationToken token = default)
+        {
+            var cachedBasket = await cache.GetStringAsync(userName, token);
+            if (!string.IsNullOrEmpty(cachedBasket))
+            {
+                return JsonSerializer.Deserialize<ShoppingCart>(json: cachedBasket)!;
+            }
+            var basket = await basketRepository.GetBasketAsync(userName, token);
+            await cache.SetStringAsync(userName, JsonSerializer.Serialize(basket), new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(10)
+            }, token);
+            return basket;
+        }
+        public async Task<ShoppingCart> StoreBasketAsync(ShoppingCart basket, CancellationToken token = default)
+        {
+            var storedBasket = await basketRepository.StoreBasketAsync(basket, token);
+            await cache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(storedBasket), new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(10)
+            }, token);
+            return storedBasket;
+        }
+        public async Task<bool> DeleteBasketAsync(string userName, CancellationToken token = default)
+        {
+            await cache.RemoveAsync(userName, token);
+            return await basketRepository.DeleteBasketAsync(userName, token);
+        }
+    }
+}
